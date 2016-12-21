6b15effe11cc2f22b33b93a215fbb64f3b4f9d20
Abdelrahman Ossama <doo2doo2@hotmail.com>
2016-12-22 00:29:03 +0200

    Test

--- GUICalculator/src/guicalculator/OperationsHandler.java
+++ GUICalculator/src/guicalculator/OperationsHandler.java
@@ -12,7 +12,35 @@ import com.wolfram.alpha.WAQuery;
 import com.wolfram.alpha.WAQueryResult;
 import com.wolfram.alpha.WASubpod;
 
-
+/*
+ * A simple example program demonstrating the WolframAlpha.jar library. The program
+ * performs a query given on the command line and prints out the resulting pod titles
+ * and plaintext content.
+ * 
+ * You will need to insert your appid into the code. To compile or run this program
+ * you will need the following dependent libraries on your classpath (including 
+ * WolframAlpha.jar, of course):
+ * 
+ *     commons-codec-1.3.jar
+ *     httpclient-4.0.1.jar
+ *     httpcore-4.0.1.jar
+ *     commons-logging.jar
+ *     
+ * These libraries are widely available on the Internet. You can probably use other version
+ * numbers than these, although these are the versions I used.
+ * 
+ * To launch from the command line, do the following (these classpath specifications assume
+ * that the WolframAlpha.jar file and the four other dependent jars listed above are in the same
+ * directory as AlphaAPISample.class):
+ * 
+ *     Windows:
+ *     
+ *       java -classpath .;* AlphaAPISample "sin x"
+ *     
+ *     Linux, Mac OSX:
+ *     
+ *       java -classpath .:* AlphaAPISample "sin x"
+ */
 /**
  *
  * @author Abdelrahman Ossama
